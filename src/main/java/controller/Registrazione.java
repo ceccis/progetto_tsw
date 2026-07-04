d5beb0e191cbedf495523cc38cb939b83be8bf34
Asia Aiello <a.aiello37@studenti.unisa.it>
2026-07-04 17:08:42 +0200

    Correzione
    aggiunta doGet nella servler di registrazione

--- src/main/java/controller/Registrazione.java
+++ src/main/java/controller/Registrazione.java
@@ -61,9 +61,8 @@ public class Registrazione extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		 request.getRequestDispatcher("/WEB-INF/views/registrazione.jsp")
+         .forward(request, response);
 	}
 
 	/**
