a7b052d6cff4247fc5cccb390b43d1344a1cb055
Asia Aiello <a.aiello37@studenti.unisa.it>
2026-07-04 16:42:47 +0200

    Fix dettagli (rimozione tabella recensione, aggiunta attributo prezzo totale (prezzo unitario + iva) ad acquisto, aumentato la lunghezza dell'attributo foto (da blob a largeblob)

--- database/database.sql
+++ database/database.sql
@@ -9,14 +9,13 @@ USE sitotsw;
 
 CREATE TABLE utenteRegistrato( 
 	id_utente INT AUTO_INCREMENT PRIMARY KEY,
-    ruolo VARCHAR(20) NOT NULL DEFAULT 'utente', -- AGGIUNTA ATTRIBUTO RUOLO CON VALORE DEFAULT UTENTE
+    ruolo VARCHAR(20) NOT NULL DEFAULT 'utente', 
     nome VARCHAR(50) NOT NULL, 
     cognome VARCHAR(50)NOT NULL,
     username VARCHAR(50) UNIQUE NOT NULL, 
     email VARCHAR(100) UNIQUE NOT NULL,
-    password_hash CHAR(128) NOT NULL, -- AUMENTATO LUNGHEZZA PER L'HASH
+    password_hash CHAR(128) NOT NULL, 
     bio VARCHAR(500),
-    metodo_pagamento VARCHAR (150) CHECK (metodo_pagamento IN ('Carta di credito', 'PayPal', 'ApplePay', 'Contrassegno')), -- AGGIUNTA METODO PAGAMENTO
     nazione VARCHAR(100) NOT NULL, 
     regione VARCHAR(100) NOT NULL,
     provincia VARCHAR(100) NOT NULL,
@@ -29,13 +28,13 @@ CREATE TABLE utenteRegistrato(
 CREATE TABLE libro (
 	id_libro INT AUTO_INCREMENT PRIMARY KEY,
     id_venditore INT NOT NULL,
-    disponibilita BOOLEAN DEFAULT TRUE, -- AGGIUNTA ATTRIBUTO DISPONIBILITA se un libro viene comprato disponibilita = false
+    disponibilita BOOLEAN DEFAULT TRUE, 
     ISBN VARCHAR(50) NOT NULL,
     nome VARCHAR(100) NOT NULL,
     autore VARCHAR(50) NOT NULL,
     genere VARCHAR(50) NOT NULL,
     prezzo DECIMAL(10,2) NOT NULL DEFAULT 0.00 CHECK (prezzo >= 0), -- con default imposto un prezzo predefinito nel caso in cui non venga inserito e con check controllo che non vegnano messi numeri negativi
-	foto BLOB, -- AGGIUNTA ATTRIBUTO FOTO
+	foto LONGBLOB, -- MODIFICA DA BLOB A LONGBLOB
     descrizione VARCHAR(500),
     data_pubblicazione DATE NOT NULL,
    
@@ -48,8 +47,9 @@ CREATE TABLE acquisto(
     id_acquirente INT NOT NULL,
     id_libro INT NOT NULL,
     -- AGGIUNTA DATI PER LA FATTURA
-    prezzo_unitario DECIMAL(10,2) NOT NULL,
+    prezzo_unitario DECIMAL(10,2) NOT NULL DEFAULT 0.00,
     iva_percentuale DECIMAL(5,2) NOT NULL DEFAULT 0.00,
+    prezzo_totale DECIMAL (20, 2) NOT NULL DEFAULT 0.00, -- AGGUIUNTA PREZZO TOTALE cioe prezzo unitario + iva
     quantita INT NOT NULL DEFAULT 1, -- default 1 perchè essendo un sito di compravendita di libri usati, non è possibile acquistare più unità di un prodotto
     data_acquisto TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- quando viene fatto un acquisto verrá inserita automaticamente la data e ora del momento in cui é stato fatto
     metodo_pagamento_usato VARCHAR(50) NOT  NULL,
@@ -58,16 +58,6 @@ CREATE TABLE acquisto(
     FOREIGN KEY (id_libro) REFERENCES libro(id_libro) ON DELETE RESTRICT, -- un libro non puó essere rimosso se ci sono degli acquisti collegati ad esso
     FOREIGN KEY (id_venditore) REFERENCES utenteRegistrato(id_utente) ON DELETE RESTRICT -- un venditore non può essere eliminato se ha effettuato una vendita
 );
-
-CREATE TABLE recensione(
-	id_acquisto INT PRIMARY KEY,
-    testo VARCHAR(500),
-    data_recensione TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    rating INT CHECK (rating BETWEEN 1 AND 5),
-   
-FOREIGN KEY (id_acquisto) REFERENCES acquisto(id_acquisto) ON DELETE CASCADE -- se un ordine viene cancellato viene cancellata anche la recensione
-);
-
 
 -- creazione tabella relazione molti a molti
 
@@ -80,6 +70,7 @@ CREATE TABLE carrello (
     FOREIGN KEY (id_libro) REFERENCES libro(id_libro) ON DELETE CASCADE
    
 );
+
 
 
 
